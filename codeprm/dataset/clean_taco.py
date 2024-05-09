06557caa00f01fd290ab1f7e1190d8fbf29bbaa3
cassanof <federico.cassano@scale.com>
2024-05-09 13:05:33 -0700

    a

--- codeprm/dataset/clean_taco.py
+++ codeprm/dataset/clean_taco.py
@@ -1,23 +1,27 @@
 import datasets
+import numpy as np
 import os
 import json
 import sys
 sys.set_int_max_str_digits(0)
 
-IO_INSTRUMENTATION = """
-
-"""
-
 ds = datasets.load_dataset("BAAI/TACO", split="train", trust_remote_code=True)
+# json load the solutions
 ds = ds.map(lambda x: {"solutions": json.loads(
     x["solutions"])}, num_proc=os.cpu_count())
 print("Original dataset size: ", len(ds))
 # filter to have at least 1 solution
 ds = ds.filter(lambda x: len(x["solutions"]) > 0)
 print("Has at least one solution: ", len(ds))
-# filter codewars, very low quality
-ds = ds.filter(lambda x: x["source"] != "codewars", num_proc=os.cpu_count())
-print("Filtered codewars: ", len(ds))
+
+# mean solution line length needs to be >= 3
+ds = ds.filter(lambda x: np.mean([len(s.strip().split("\n")) for s in x["solutions"]]) >= 3, num_proc=os.cpu_count())
+print("After mean solution line length >= 3: ", len(ds))
+
+
+# need to have enough tests, at least 10
+ds = ds.filter(lambda x: len(json.loads(x["input_output"])["inputs"]) >= 10, num_proc=os.cpu_count())
+print("After having at least 10 tests: ", len(ds))
 
 total_size = 0
 for ex in ds:
@@ -25,4 +29,5 @@ for ex in ds:
 
 print("Total number of solutions: ", total_size)
 
-
+# save the dataset
+ds.save_to_disk("taco_cleaned")
