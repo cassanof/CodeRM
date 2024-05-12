7e9e235969835168996f1a0b9a801c855dd96467
cassanof <federico.cassano@federico.codes>
2024-05-12 14:40:14 -0700

    mechanism with restart

--- codeprm/dataset/filter_not_executing.py
+++ codeprm/dataset/filter_not_executing.py
@@ -1,4 +1,6 @@
 import datasets
+from codeprm.utils import chunkify
+from tqdm import tqdm
 import json
 from codeprm.execution import smart_exec_tests
 import os
@@ -7,9 +9,11 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--input_dir", type=str, default="./taco_cleaned")
 parser.add_argument("--executor", type=str, default="http://127.0.0.1:8000")
+parser.add_argument("--container_name", type=str, default="code-exec")
 parser.add_argument("--max-solns", type=int, default=75)
 parser.add_argument("--max-attempts", type=int, default=100)
 parser.add_argument("--workers", type=int, default=os.cpu_count() - 1)
+parser.add_argument("--batch-size", type=int, default=os.cpu_count() * 2)
 parser.add_argument("--sample", type=int, default=None)
 args = parser.parse_args()
 
@@ -17,6 +21,16 @@ ds = datasets.load_from_disk("./taco_cleaned")
 
 if args.sample is not None:
     ds = ds.select(range(args.sample))
+
+ds = ds.to_list()
+print("Loaded dataset: ", len(ds))
+chunks = chunkify(ds, args.batch_size)
+
+dses = []
+for chunk in chunks:
+    dses.append(datasets.Dataset.from_list(chunk))
+
+print(f"Loaded dataset chunks (batch size: {args.batch_size}): {len(dses)}")
 
 
 def filter_not_executing(ex):
@@ -38,7 +52,23 @@ def filter_not_executing(ex):
     }
 
 
-ds = ds.map(filter_not_executing, num_proc=args.workers)
-ds = ds.filter(lambda x: len(x["solutions"]) > 0)
+def docker_restart():
+    import subprocess
+    p = subprocess.Popen(
+        ["docker", "restart", args.container_name], stdout=subprocess.PIPE)
+    p.communicate()
+    return p.returncode
 
-ds.save_to_disk(args.input_dir + "_exec_filtered")
+
+final_ds = []
+
+for ds in tqdm(dses, desc="Filtering all datasets"):
+    ds = ds.map(filter_not_executing, num_proc=args.workers)
+    ds = ds.filter(lambda x: len(x["solutions"]) > 0)
+    final_ds.extend(ds.to_list())
+    # restart docker container
+    print("Done with a batch, restarting docker container for stability...")
+    docker_restart()
+
+final_ds = datasets.Dataset.from_list(final_ds)
+final_ds.save_to_disk(args.input_dir + "_exec_filtered")
