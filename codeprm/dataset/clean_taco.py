ee31a2187399f87ac729c8c32dd8e20a24320ea3
cassanof <federico.cassano@federico.codes>
2024-05-11 23:20:30 -0700

    analysis

--- codeprm/dataset/clean_taco.py
+++ codeprm/dataset/clean_taco.py
@@ -5,6 +5,20 @@ import json
 import sys
 import ast
 
+from transformers import AutoTokenizer
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--output", type=str, default="taco_cleaned")
+parser.add_argument("--min_solution_line_length", type=int, default=3)
+parser.add_argument("--min_tests", type=int, default=10)
+parser.add_argument("--min_solutions", type=int, default=1)
+parser.add_argument("--max_tokens", type=int, default=2048)
+parser.add_argument("--split", type=str, default="train")
+args = parser.parse_args()
+
+tokenizer = AutoTokenizer.from_pretrained("bigcode/starcoder2-15b")
+
 if hasattr(sys, "set_int_max_str_digits"):
     sys.set_int_max_str_digits(0)
 
@@ -17,7 +31,8 @@ def does_parse(code):
         return False
 
 
-ds = datasets.load_dataset("BAAI/TACO", split="train", trust_remote_code=True)
+ds = datasets.load_dataset(
+    "BAAI/TACO", split=args.split, trust_remote_code=True)
 # json load the solutions
 ds = ds.map(lambda x: {"solutions": json.loads(
     x["solutions"])}, num_proc=os.cpu_count())
@@ -25,20 +40,25 @@ print("Original dataset size: ", len(ds))
 
 # mean solution line length needs to be >= 3
 ds = ds.filter(lambda x: np.mean([len(s.strip().split("\n"))
-               for s in x["solutions"]]) >= 3, num_proc=os.cpu_count())
+               for s in x["solutions"]]) >= args.min_solution_line_length, num_proc=os.cpu_count())
 print("After mean solution line length >= 3: ", len(ds))
 
 
 # need to have enough tests, at least 10
 ds = ds.filter(lambda x: len(json.loads(x["input_output"])[
-               "inputs"]) >= 10, num_proc=os.cpu_count())
+               "inputs"]) >= args.min_tests, num_proc=os.cpu_count())
 print("After having at least 10 tests: ", len(ds))
 
-# remove any solution that is not parsable
+# strip all solutions
+ds = ds.map(lambda x: {"solutions": [s.strip()
+            for s in x["solutions"]]}, num_proc=os.cpu_count())
+
+# remove any solution that is not parsable or is too long
 ds = ds.map(lambda x: {"solutions": [
-            s for s in x["solutions"] if does_parse(s)]}, num_proc=os.cpu_count())
+            s for s in x["solutions"] if does_parse(s) and len(tokenizer.encode(s)) <= args.max_tokens]}, num_proc=os.cpu_count())
+
 # filter to have at least 1 solution
-ds = ds.filter(lambda x: len(x["solutions"]) > 0)
+ds = ds.filter(lambda x: len(x["solutions"]) >= args.min_solutions)
 print("Has at least one solution: ", len(ds))
 
 
@@ -78,4 +98,4 @@ for ex in ds:
 print("Total number of solutions: ", total_size)
 
 # save the dataset
-ds.save_to_disk("taco_cleaned")
+ds.save_to_disk(args.output)
