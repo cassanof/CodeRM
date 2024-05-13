5ce3b4889c1004800b012c3dd9ea4f919e7124cc
cassanof <federico.cassano@federico.codes>
2024-05-12 22:58:18 -0700

    dedup

--- codeprm/dataset/format_to_train.py
+++ codeprm/dataset/format_to_train.py
@@ -7,18 +7,20 @@ def main(args):
     print("Original ds:")
     print(ds)
     content = []
+    solutions = []
     mark = args.mask_loss_mark if args.mask_loss_mark else ""
     for ex in ds:
         for sol in ex["solutions"]:
             content.append(py_prompt(ex["question"], mark + sol))
+            solutions.append(sol)
 
-    ds = datasets.Dataset.from_dict({"content": content})
+    ds = datasets.Dataset.from_dict({"content": content, "solutions": solutions})
     print("New ds:")
     print(ds)
     print("Printing one example:")
     print(ds[0]["content"])
     ds.push_to_hub(args.push, private=True)
-    print("IMPORTANT: Remember to MinHash-dedup the dataset before training!")
+    print("IMPORTANT: Remember to MinHash-dedup the dataset before training! Only dedup based on 'solutions' column.")
 
 
 if __name__ == "__main__":
