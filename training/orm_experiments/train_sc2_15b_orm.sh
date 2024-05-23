3a62a53f0c7f010b438216393b0838ccce90a83b
cassanof <federico.cassano@federico.codes>
2024-05-22 23:47:36 -0700

    more scripts

---
+++ training/orm_experiments/train_sc2_15b_orm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export WANDB_PROJECT="codeprm"
+export WANDB_NAME="starcoder2_15b_orm_raw10"
+pushd ../code-scorer/
+python3 -m torch.distributed.launch \
+  --nproc_per_node 8 \
+  train.py \
+  --seq_len 4096 \
+  --batch_size 4 \
+  --gradient_accumulation_steps 1 \
+  --epochs 20 \
+  --lr 2e-5 \
+  --weight_decay 0.01 \
+  --save_dir "./model_starcoder2_15b_orm_raw10" \
+  --dataset "cassanof/taco_orm_raw10" \
+  --eval_dataset "cassanof/lcb_orm_eval" \
+  --model "bigcode/starcoder2-15b" \
+  --bf16 \
+  --no_fp16 \
+  --fa2
+popd
