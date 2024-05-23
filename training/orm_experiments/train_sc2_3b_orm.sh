2fc76eb62163f33af8a2806b279777f39a6598e5
cassanof <federico.cassano@federico.codes>
2024-05-22 23:46:49 -0700

    more epochs

--- training/orm_experiments/train_sc2_3b_orm.sh
+++ training/orm_experiments/train_sc2_3b_orm.sh
@@ -9,7 +9,7 @@ python3 -m torch.distributed.launch \
   --seq_len 4096 \
   --batch_size 4 \
   --gradient_accumulation_steps 1 \
-  --epochs 10 \
+  --epochs 20 \
   --lr 2e-5 \
   --weight_decay 0.01 \
   --save_dir "./model_starcoder2_3b_orm_raw10" \
