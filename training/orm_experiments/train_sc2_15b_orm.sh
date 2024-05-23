818803fde27212fcaf9d9543fa83ccd1e02486aa
cassanof <federico.cassano@federico.codes>
2024-05-23 00:22:30 -0700

    new scripts

--- training/orm_experiments/train_sc2_15b_orm.sh
+++ training/orm_experiments/train_sc2_15b_orm.sh
@@ -7,11 +7,12 @@ python3 -m torch.distributed.launch \
   --nproc_per_node 8 \
   train.py \
   --seq_len 4096 \
-  --batch_size 4 \
-  --gradient_accumulation_steps 1 \
+  --batch_size 1 \
+  --gradient_accumulation_steps 4 \
   --epochs 20 \
   --lr 2e-5 \
   --weight_decay 0.01 \
+  --attention_dropout 0.1 \
   --save_dir "./model_starcoder2_15b_orm_raw10" \
   --dataset "cassanof/taco_orm_raw10" \
   --eval_dataset "cassanof/lcb_orm_eval" \
