c7a56d90dd3f6af5dc90e1eb323757a63a4dedbf
cassanof <federico.cassano@scale.com>
2024-05-24 10:24:48 -0700

    remove large batch

--- training/orm_experiments/generate_orm_dataset.sh
+++ training/orm_experiments/generate_orm_dataset.sh
@@ -5,7 +5,6 @@ python3 codeprm/eval/taco_eval.py \
   --split train \
   --model cassanof/starcoder2-15b-taco-reasoning \
   --num-gpus 8 \
-  --batch-size 4096 \
   --completion-limit 100 \
   --temperature 0.8 \
   --output ./codeprm/dataset/cassanof/orm_dataset_raw100_og \
