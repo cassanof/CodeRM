daf162af49f63f1ab0b4a690e7cbc773b890790f
cassanof <federico.cassano@federico.codes>
2024-05-27 01:45:57 -0700

    add tab to space step

--- codeprm/dataset/clean_reasoning_steps.py
+++ codeprm/dataset/clean_reasoning_steps.py
@@ -95,6 +95,23 @@ def get_steps_to_char_ratio(code):
     return reasoning_steps / characters_count if characters_count > 0 else 0
 
 
+def attempt_convert_tabs_to_spaces(code):
+    """Attempt to convert tabs to spaces in the code. Uses ast.parse to make sure the code is valid."""
+    import ast
+
+    converted_code = []
+    for line in code.split('\n'):
+        converted_code.append(line.expandtabs(4))
+
+    converted = '\n'.join(converted_code)
+    try:
+        ast.parse(converted)
+        return converted
+    except SyntaxError:
+        print("Syntax error after converting tabs to spaces.")
+        return code
+
+
 def main(args):
     ds = datasets.load_dataset(args.dataset, split="train")
     ds = ds.map(lambda x: {
@@ -104,6 +121,10 @@ def main(args):
     # this is based on visual inspection of the data (see taco_reasoning_analysis.ipynb)
     ds = ds.map(lambda x: {
         "reasoning_steps": [r for r in x["reasoning_steps"] if 0.01 <= get_steps_to_char_ratio(r) <= 0.8],
+    })
+    # convert tabs to spaces
+    ds = ds.map(lambda x: {
+        "reasoning_steps": [attempt_convert_tabs_to_spaces(r) for r in x["reasoning_steps"]],
     })
     ds = ds.filter(lambda x: len(x["reasoning_steps"]) > 0)
     ds.save_to_disk(args.output)
