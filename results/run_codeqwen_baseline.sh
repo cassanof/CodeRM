3541c13d53d01610821b745826f2dd29da77bbe0
cassanof <federico.cassano@scale.com>
2024-05-30 16:47:10 +0000

    l

---
+++ results/run_codeqwen_baseline.sh
@@ -0,0 +1,2 @@
+python3 codeprm/eval/livecodebench_eval.py --model Qwen/CodeQwen1.5-7B --model-kind few-shot --output ./results/codeqwen_rerun/lcb_codeqwen_7b_greedy;
+python3 codeprm/eval/livecodebench_eval.py --model Qwen/CodeQwen1.5-7B --model-kind few-shot --temperature 0.8 --completion-limit 100 --output ./results/codeqwen_rerun/lcb_codeqwen_7b_temp08;
---
+++ results/run_llama3_baselines.sh
@@ -0,0 +1,4 @@
+python3 codeprm/eval/livecodebench_eval.py --model meta-llama/Meta-Llama-3-8B --model-kind few-shot --output ./results/greedy_baselines/lcb_llama3_8b_greedy;
+python3 codeprm/eval/livecodebench_eval.py --model meta-llama/Meta-Llama-3-70B --model-kind few-shot --output ./results/greedy_baselines/lcb_llama3_70b_greedy --num-gpus 4;
+python3 codeprm/eval/livecodebench_eval.py --model meta-llama/Meta-Llama-3-8B --model-kind few-shot --temperature 0.8 --completion-limit 100 --output  ./results/temp_baselines/lcb_llama3_8b_temp08;
+python3 codeprm/eval/livecodebench_eval.py --model meta-llama/Meta-Llama-3-70B --model-kind few-shot --temperature 0.8 --completion-limit 100 --output ./results/temp_baselines/lcb_llama3_70b_temp08 --num-gpus 4;
