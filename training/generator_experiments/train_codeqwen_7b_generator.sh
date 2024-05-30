cdf1e232f7658f614791dc7239674ae887f2253a
cassanof <federico.cassano@scale.com>
2024-05-29 22:54:13 -0700

    train

--- training/generator_experiments/train_codeqwen_7b_generator.sh
+++ training/generator_experiments/train_codeqwen_7b_generator.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied. Please give deepspeed config file as argument"
-    exit 1
-fi
-DS=$(realpath $1)
 export WANDB_PROJECT="codeprm"
 export WANDB_NAME=$(basename $0 .sh)
 OUTDIR="./model_codeqwen_7b_generator"
@@ -13,7 +7,6 @@ pushd ../finetuning-harness/
 python3 -m torch.distributed.launch \
         --nproc_per_node 8 \
         main.py \
-        --deepspeed="$DS" \
         --model_path="Qwen/CodeQwen1.5-7B" \
         --dataset_name="codegenning/finetuning-set-codeqwen-v0" \
         --dataset_loader="padded" \
