a5240ee8a02fe6a877d208c5f9edc4332203da67
cassanof <federico.cassano@scale.com>
2024-05-31 01:00:54 -0700

    temp

--- results/run_temperature_experiment.sh
+++ results/run_temperature_experiment.sh
@@ -1,10 +1,12 @@
-TEMPS=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0)
-mkdir ./temperature_experiment
-for TEMP in ${TEMPS[@]}; do
-  python3 codeprm/eval/livecodebench_eval.py \
-    --model "codegenning/generator-sc2-15b" \
-    --temperature $TEMP \
-    --completion-limit 250 \
-    --num-gpus 4 \
-    --output ./temperature_experiment/lcb_sc2_15b_generator_temp${TEMP};
-done
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 TEMPERATURE"
+  exit 1
+fi
+TEMP=$1
+mkdir -p ./temperature_experiment
+python3 ../codeprm/eval/livecodebench_eval.py \
+  --model "codegenning/generator-sc2-15b" \
+  --temperature $TEMP \
+  --completion-limit 250 \
+  --num-gpus 4 \
+  --output ./temperature_experiment/lcb_sc2_15b_generator_temp${TEMP};
