ab61d1a863c47d47f9197e8b79d77d4ed2522b80
cassanof <federico.cassano@scale.com>
2024-05-31 01:09:42 -0700

    conta

---
+++ results/run_contaminated_split.sh
@@ -0,0 +1,6 @@
+python3 ../codeprm/eval/livecodebench_eval.py \
+  --model "codegenning/generator-sc2-15b" \
+  --dataset "cassanof/livecodebench_lite_contaminated" \
+  --temperature 0.8 \
+  --completion-limit 100 \
+  --output ./temp_generator/lcb_contaminated_generator_sc2_15b
