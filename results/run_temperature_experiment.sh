72fcb45377069a8d98a8fab20694d90610ac9325
cassanof <federico.cassano@scale.com>
2024-05-31 00:58:27 -0700

    temp

--- results/run_temperature_experiment.sh
+++ results/run_temperature_experiment.sh
@@ -2,7 +2,7 @@ TEMPS=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0)
 mkdir ./temperature_experiment
 for TEMP in ${TEMPS[@]}; do
   python3 codeprm/eval/livecodebench_eval.py \
-    --model "<TODO>" \
+    --model "codegenning/generator-sc2-15b" \
     --temperature $TEMP \
     --completion-limit 250 \
     --num-gpus 4 \
