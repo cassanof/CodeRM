35ee07150ea040aafbb8d0a4bd90c315fe6ecf82
cassanof <federico.cassano@scale.com>
2024-06-03 23:29:31 -0700

    no offload

--- training/orm_experiments/train_sc2_7b_orm_mutated.sh
+++ training/orm_experiments/train_sc2_7b_orm_mutated.sh
@@ -16,7 +16,7 @@ python3 -m torch.distributed.launch \
   --dataset "codegenning/orm_dataset_mutated" \
   --eval_dataset "codegenning/orm_eval_dataset" \
   --model "<MODEL HERE>" \
-  --deepspeed "./deepspeed_cfgs/offload_cpu_only_optimizer.json" \
+  --deepspeed "./deepspeed_cfgs/no_offload.json" \
   --num_labels 2 \
   --bf16 \
   --no_fp16 \
