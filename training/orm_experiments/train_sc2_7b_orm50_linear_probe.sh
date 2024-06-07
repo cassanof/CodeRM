8d39de9d8741e66d3108dbee7102f4e827647290
cassanof <federico.cassano@scale.com>
2024-06-06 22:25:25 -0700

    high lr

--- training/orm_experiments/train_sc2_7b_orm50_linear_probe.sh
+++ training/orm_experiments/train_sc2_7b_orm50_linear_probe.sh
@@ -10,7 +10,7 @@ python3 -m torch.distributed.launch \
   --batch_size 1 \
   --gradient_accumulation_steps 32 \
   --epochs 3 \
-  --lr 1e-5 \
+  --lr 0.03 \
   --weight_decay 0.01 \
   --save_dir "./model_starcoder2_7b_orm50_linear_probe" \
   --dataset "codegenning/orm_dataset_raw50" \
