bac8ffdf332938b1122cc33fdfdc35af8989e820
cassanof <federico.cassano@federico.codes>
2024-06-14 00:33:51 -0700

    a

--- training/rl/train_rloo_tldr_demo.py
+++ training/rl/train_rloo_tldr_demo.py
@@ -1,7 +1,7 @@
 import multiprocessing
 import shutil
-import wandb
 import os
+from generic import MakeShiftWandbCallback
 
 from datasets import load_dataset
 from transformers import (
@@ -9,10 +9,6 @@ from transformers import (
     AutoModelForSequenceClassification,
     AutoTokenizer,
     HfArgumentParser,
-    TrainerCallback,
-    TrainerControl,
-    TrainerState,
-    TrainingArguments,
 )
 
 from trl import ModelConfig
@@ -51,59 +47,6 @@ accelerate launch --config_file examples/accelerate_configs/deepspeed_zero2.yaml
     --non_eos_penalty \
     --stop_token eos \
 """
-
-
-class MakeShiftWandbCallback(TrainerCallback):
-    def __init__(self):
-        self._initialized = False
-
-    def setup(
-            self,
-            args: TrainingArguments,
-            state: TrainerState,
-    ):
-        trial_name = state.trial_name
-        init_args = {}
-        if trial_name is not None:
-            init_args["name"] = trial_name
-            init_args["group"] = args.run_name
-        else:
-            if not (args.run_name is None or args.run_name == args.output_dir):
-                init_args["name"] = args.run_name
-
-        if state.is_local_process_zero:
-            config = args.to_dict()
-            for key in model_config.to_dict():
-                if key not in config:
-                    config[key] = model_config.to_dict()[key]
-
-            wandb.init(
-                project=os.getenv("WANDB_PROJECT", "rl"),
-                **init_args,
-                config=config,
-            )
-            wandb.define_metric("train/global_step")
-            wandb.define_metric(
-                "*", step_metric="train/global_step", step_sync=True)
-
-    def on_log(
-            self,
-            args: TrainingArguments,
-            state: TrainerState,
-            control: TrainerControl,
-            logs=None,
-            **kwargs,
-    ):
-        if not self._initialized:
-            self.setup(args, state)
-            self._initialized = True
-
-        if logs is None:
-            logs = {}
-
-        if state.is_world_process_zero:
-            wandb.log(
-                {**logs, "train/global_step": state.global_step})
 
 
 if __name__ == "__main__":
