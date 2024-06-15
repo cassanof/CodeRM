b6470afc250135357367e66a8bb1883aa7a8ef6f
cassanof <federico.cassano@scale.com>
2024-06-14 18:48:50 -0700

    test cfg

--- training/rl/train_sc2_7b_rl_testing.sh
+++ training/rl/train_sc2_7b_rl_testing.sh
@@ -1,16 +1,16 @@
-BASE_MODEL="/mnt/efs/federicocassano/codeprm/training/finetuning-harness/model_starcoder2_7b_generator/checkpoint-1068"
+BASE_MODEL="codegenning/generator-sc2-7b"
 # TODO: switch out to BCE model once code is good
-REWARD_MODEL="/mnt/efs/federicocassano/codeprm/training/code-scorer/model_starcoder2_7b_orm50_mse/checkpoint-1782"
+REWARD_MODEL="codegenning/orm-sc2-7b-mse-v0"
 OUTDIR="./sc2_7b_rl_testing"
 
-CUDA_VISIBLE_DEVICES="4,5,6,7" accelerate launch --config_file ./z3.yaml --gpu_ids 4,5,6,7 --num_processes 4 train_rloo.py \
+accelerate launch --config_file ./z2.yaml --num_processes 8 train_rloo.py \
     --output_dir $OUTDIR \
     --num_ppo_epochs 2 \
     --num_mini_batches 2 \
     --learning_rate 3e-6 \
     --per_device_train_batch_size 1 \
-    --gradient_accumulation_steps 16 \
-    --total_episodes 100_000 \
+    --gradient_accumulation_steps 8 \
+    --total_episodes 25_000 \
     --model_name_or_path $BASE_MODEL \
     --sft_model_path $BASE_MODEL \
     --reward_model_path $REWARD_MODEL \
@@ -21,5 +21,4 @@ CUDA_VISIBLE_DEVICES="4,5,6,7" accelerate launch --config_file ./z3.yaml --gpu_i
     --stop_token eos \
     --kl_coef 0.03 \
     --logging_steps 1 \
-    --num_sample_generation 0 \
-    --logging_first_step False
+    --num_sample_generation 5
