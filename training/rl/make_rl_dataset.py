161b32cfa9e5e3ffdd9733fe118da05e75967184
cassanof <federico.cassano@scale.com>
2024-06-17 12:46:18 -0700

    use full taco dataset

--- training/rl/make_rl_dataset.py
+++ training/rl/make_rl_dataset.py
@@ -1,4 +1,5 @@
 import datasets
+from transformers import AutoTokenizer
 from codeprm.prompts import py_prompt
 
 ORDER = ["EASY", "MEDIUM", "MEDIUM_HARD", "HARD", "VERY_HARD"]
@@ -6,7 +7,26 @@ ORDER = ["EASY", "MEDIUM", "MEDIUM_HARD", "HARD", "VERY_HARD"]
 
 def main(args):
     train_dataset = datasets.load_dataset(
-        args.train, split="train")
+        "BAAI/TACO", split="train+test", trust_remote_code=True)
+    tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
+
+    # dedup by question
+    print("Deduping by question. Before: ", len(train_dataset))
+    new_train = []
+    qset = set()
+    for ex in train_dataset:
+        q = ex["question"]
+        if q not in qset:
+            new_train.append(ex)
+            qset.add(q)
+    train_dataset = datasets.Dataset.from_list(new_train)
+    print("After dedup: ", len(train_dataset))
+
+    # filter out long examples
+    train_dataset = train_dataset.filter(
+        lambda x: len(tokenizer.encode(x["question"])) < args.max_tokens)
+    print("After filtering for max length: ", len(train_dataset))
+
     test_dataset = datasets.load_dataset(
         args.test, split="test")
 
@@ -59,7 +79,7 @@ def main(args):
         "test": datasets.Dataset.from_list(test_fmt),
     }
     ds = datasets.DatasetDict(ds)
-    ds.push_to_hub(args.push, private=True)
+    # ds.push_to_hub(args.push, private=True)
 
 
 if __name__ == "__main__":
@@ -69,6 +89,9 @@ if __name__ == "__main__":
                         default="cassanof/taco_cleaned_all")
     parser.add_argument("--test", type=str,
                         default="cassanof/livecodebench_lite_contaminated")
+    parser.add_argument("--max-tokens", type=int, default=2048)
+    parser.add_argument("--tokenizer", type=str,
+                        default="bigcode/starcoder2-15b")
     parser.add_argument("--push", type=str, default="codegenning/taco-rl")
     parser.add_argument("--order", type=str, choices=["random", "curriculum"],
                         default="random")
