662df5bef9fa3ef21e00b6e2051ec6e4dfb23266
cassanof <federico.cassano@scale.com>
2024-06-16 22:54:54 -0700

    curr

--- training/rl/make_rl_dataset.py
+++ training/rl/make_rl_dataset.py
@@ -1,6 +1,8 @@
 import datasets
 from codeprm.prompts import py_prompt
 
+ORDER = ["EASY", "MEDIUM", "MEDIUM_HARD", "HARD", "VERY_HARD"]
+
 
 def main(args):
     train_dataset = datasets.load_dataset(
@@ -8,8 +10,31 @@ def main(args):
     test_dataset = datasets.load_dataset(
         args.test, split="test")
 
-    # shuffle to avoid bias
-    train_dataset = train_dataset.shuffle()
+    if args.order == "curriculum":
+        new_train = []
+        print("Curriculum order")
+        for diff in ORDER:
+            fil = train_dataset.filter(
+                lambda x: x["difficulty"] == diff).to_list()
+            print(f"{diff}: {len(fil)}")
+            new_train.extend(fil)
+
+        # evenly distribute UNKNOWN_DIFFICULTY
+        unknown = train_dataset.filter(
+            lambda x: x["difficulty"] == "UNKNOWN_DIFFICULTY").to_list()
+        print("Adding unknown difficulty: ", len(unknown))
+        for i, ex in enumerate(unknown):
+            new_train.insert(i * (len(new_train) // len(unknown)), ex)
+
+        train_dataset = datasets.Dataset.from_list(new_train)
+
+    elif args.order == "random":
+        # shuffle to avoid bias
+        train_dataset = train_dataset.shuffle()
+    else:
+        raise ValueError(f"Unknown order: {args.order}")
+
+    # just shuffle test set
     test_dataset = test_dataset.shuffle()
 
     train_fmt = []
@@ -45,5 +70,7 @@ if __name__ == "__main__":
     parser.add_argument("--test", type=str,
                         default="cassanof/livecodebench_lite_contaminated")
     parser.add_argument("--push", type=str, default="codegenning/taco-rl")
+    parser.add_argument("--order", type=str, choices=["random", "curriculum"],
+                        default="random")
     args = parser.parse_args()
     main(args)
