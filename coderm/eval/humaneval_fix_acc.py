462707f1a8253b8a78ed0a40e8fdf0f4a9561faa
cassanof <federico.cassano@scale.com>
2024-06-26 16:45:33 -0700

    acc

---
+++ coderm/eval/humaneval_fix_acc.py
@@ -0,0 +1,80 @@
+from typing import Optional, Tuple
+from coderm.model import OutcomeRewardModel
+import datasets
+
+
+def format_ex(ex) -> Tuple[str, str]:  # passing, failing
+    p = f'''"""\n{ex["instruction"]}\n"""\n{ex["declaration"]}'''
+    buggy = p + f'{ex["buggy_solution"]}'
+    correct = p + f'{ex["canonical_solution"]}'
+    return correct, buggy
+
+
+def model_factory(kind: str, name: str):
+    if kind == "orm":
+        return OutcomeRewardModel(name)
+    else:
+        raise ValueError(f"Unknown model kind: {kind}")
+
+
+def predicted_passing(model, kind, code) -> bool:
+    if kind == "orm":
+        neg, pos = model.score([code])[0]
+        return pos > neg
+    else:
+        raise ValueError(f"Unknown model kind: {kind}")
+
+
+def main(args):
+    ds = datasets.load_dataset("bigcode/humanevalpack", "python", split="test")
+    bug_types = set(ds["bug_type"])
+
+    model = model_factory(args.model_kind, args.model)
+
+    tp = 0
+    # track which bug types are being misclassified
+    fp = {bt: 0 for bt in bug_types}
+    # track which ones are good!
+    tn = {bt: 0 for bt in bug_types}
+    fn = 0
+
+    def update(code, bug: Optional[str]):
+        nonlocal tp
+        nonlocal fn
+        pred = predicted_passing(model, args.model_kind, code)
+        if pred:
+            if bug:
+                tp += 1
+            else:
+                fp[bug] += 1
+        else:
+            if bug:
+                fn += 1
+            else:
+                tn[bug] += 1
+
+    for ex in ds:
+        c, b = format_ex(ex)
+        update(c, None)
+        update(b, ex["bug_type"])
+
+    print(f"TP: {tp}")
+    print(f"FP: {fp}")
+    print(f"TN: {tn}")
+    print(f"FN: {fn}")
+    print(
+        f"Accuracy: {(tp + sum(tn.values())) / (tp + sum(tn.values()) + sum(fp.values()) + fn)}")
+    # per-bug type accuracy
+    print("--- Per-bug type accuracy ---")
+    for bt in bug_types:
+        print(f"{bt}: {tn[bt] / (tn[bt] + fp[bt])}")
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model-kind", type=str,
+                        default="orm", choices=["orm"])
+    parser.add_argument("--model", required=True, type=str)
+    args = parser.parse_args()
+    main(args)
