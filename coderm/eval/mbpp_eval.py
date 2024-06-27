7f64f29626675659f7f78c4f8afe28c33b7d10d2
cassanof <federico.cassano@scale.com>
2024-06-26 23:53:39 -0700

    mbpp eval

---
+++ coderm/eval/mbpp_eval.py
@@ -0,0 +1,27 @@
+from coderm.eval.generic import get_generic_argparser, make_items_from_ds, generic_eval_main
+import json
+import datasets
+
+
+def main(args):
+    dataset = datasets.load_dataset(args.dataset, split=args.split).to_list()
+    items = make_items_from_ds(
+        dataset,
+        "question",
+        "test",
+        starter_code_col="starter_code",
+        difficulty_col=None,
+        random_sample=args.random_sample,
+        unique_name_col="task_id",
+    )
+    generic_eval_main(
+        args,
+        items,
+        default_timeout=120,  # lots of tests....
+    )
+
+
+if __name__ == "__main__":
+    parser = get_generic_argparser("cassanof/mbppplus_formatted")
+    args = parser.parse_args()
+    main(args)
