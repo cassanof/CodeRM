7fcc54cf0727044608fdab7dd6ed74681bfb2851
cassanof <federico.cassano@scale.com>
2024-07-06 23:48:51 -0700

    3b

---
+++ training/orm_experiments/train_sc2_3b_orm50_cot.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export WANDB_PROJECT="coderm-orm"
+export WANDB_NAME="starcoder2_3b_orm50_cot"
+pushd ../code-scorer/
+python3 -m torch.distributed.launch \
+  --nproc_per_node 8 \
+  train.py \
+  --seq_len 4096 \
+  --batch_size 1 \
+  --gradient_accumulation_steps 32 \
+  --epochs 3 \
+  --lr 1e-5 \
+  --weight_decay 0.01 \
+  --save_dir "./model_starcoder2_3b_orm50_cot" \
+  --dataset "codegenning/orm_dataset_raw50_cot" \
+  --eval_dataset "codegenning/orm_eval_dataset" \
+  --model "codegenning/generator-sc2-3b-cot" \
+  --deepspeed "./deepspeed_cfgs/no_offload.json" \
+  --num_labels 2 \
+  --bf16 \
+  --no_fp16 \
+  --fa2
+popd
