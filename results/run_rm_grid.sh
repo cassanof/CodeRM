00834885c903276b5588cfb5a43ea4cac0a4266b
cassanof <federico.cassano@federico.codes>
2024-07-10 22:03:17 -0700

    omg

--- results/run_rm_grid.sh
+++ results/run_rm_grid.sh
@@ -33,7 +33,7 @@ for rm in "${RMS[@]}"; do
     echo "Running RM $rm on input file $input_file - writing to $outfile"
     if [ "$rm" == "codegenning/orm-llama3-70b-v0" ]; then
       echo "running llama model"
-      CUDA_VISIBLE_DEVICES=$MULTI_GPU python3 ./coderm/eval/run_orm.py --model "$rm" --input "$input_file_no_gz" --output "$outfile" --device "auto"
+      CUDA_VISIBLE_DEVICES=$MULTI_GPU python3 ./coderm/eval/run_orm.py --model "$rm" --input "$input_file" --output "$outfile" --device "auto"
     else
       echo "running starcoder model"
       CUDA_VISIBLE_DEVICES=$SINGLE_GPU python3 ./coderm/eval/run_orm.py --model "$rm" --input "$input_file" --output "$outfile"
