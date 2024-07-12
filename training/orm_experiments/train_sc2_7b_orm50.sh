cf4046e5ec1a3fdf3fcd6dabbf5184818e37e605
cassanof <federico.cassano@scale.com>
2024-07-11 17:06:42 -0700

    7b rerun

--- training/orm_experiments/train_sc2_7b_orm50.sh
+++ training/orm_experiments/train_sc2_7b_orm50.sh
@@ -4,18 +4,18 @@ export WANDB_PROJECT="coderm-orm"
 export WANDB_NAME="starcoder2_7b_orm50"
 pushd ../code-scorer/
 python3 -m torch.distributed.launch \
-  --nproc_per_node 4 \
+  --nproc_per_node 8 \
   train.py \
   --seq_len 4096 \
-  --batch_size 1 \
-  --gradient_accumulation_steps 32 \
+  --batch_size 2 \
+  --gradient_accumulation_steps 16 \
   --epochs 3 \
   --lr 1e-5 \
   --weight_decay 0.01 \
-  --save_dir "./model_starcoder2_7b_orm50_og" \
+  --save_dir "./model_starcoder2_7b_orm50" \
   --dataset "codegenning/orm_dataset_raw50" \
   --eval_dataset "codegenning/orm_eval_dataset" \
-  --model "/mnt/efs/federicocassano/coderm/training/finetuning-harness/model_starcoder2_7b_generator/checkpoint-1068" \
+  --model "codegenning/generator-sc2-7b" \
   --deepspeed "./deepspeed_cfgs/no_offload.json" \
   --num_labels 2 \
   --bf16 \
